fdc04707c336c7c28b3b85824c03831feb72c89e
psmigielski <walek.smigielski@gmail.com>
2022-12-26 21:26:08 +0100

    add play command and add handling for basic distube events

--- bot/src/classes/DiscordBot.ts
+++ bot/src/classes/DiscordBot.ts
@@ -1,12 +1,22 @@
-import DisTube, {DisTubeOptions} from "distube";
-import {Client, Collection, Events, GatewayIntentBits, REST, Routes} from "discord.js";
-import {YtDlpPlugin} from "@distube/yt-dlp";
+import DisTube, {DisTubeOptions, Playlist, SearchResult} from "distube";
+import {
+    Client,
+    Collection, EmbedBuilder,
+    Events,
+    GatewayIntentBits,
+    GuildTextBasedChannel,
+    IntentsBitField,
+    Message,
+    REST,
+    Routes
+} from "discord.js";
+import { YtDlpPlugin } from "@distube/yt-dlp";
 import { SoundCloudPlugin } from "@distube/soundcloud";
-import { SpotifyPlugin } from "@distube/spotify";
-import dotenv from "dotenv";
+import { SpotifyPlugin } from '@distube/spotify';
 import {CommandType} from "../types/CommandType";
 import path from "node:path";
 import fs from "node:fs";
+import { Queue,  Song} from "distube";
 export class DiscordBot extends Client{
     public disTube: DisTube;
     public token: string;
@@ -15,13 +25,20 @@ export class DiscordBot extends Client{
     private guildIds: Array<string>;
     private disTubeOptions: DisTubeOptions = {
         nsfw: true,
-        plugins: [new YtDlpPlugin({update: true}), new SoundCloudPlugin(), new SpotifyPlugin()],
+        leaveOnEmpty: true,
+        emptyCooldown: 30,
+        leaveOnFinish: false,
         emitNewSongOnly: true,
-        searchSongs: 10
+        plugins: [new YtDlpPlugin({update: true}), new SoundCloudPlugin(), new SpotifyPlugin()],
+        searchSongs: 5,
+        youtubeCookie: process.env.YOUTUBE_COOKIE as string
     }
     constructor() {
-        super({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
-        dotenv.config();
+        super({ intents: [
+            IntentsBitField.Flags.Guilds,
+            IntentsBitField.Flags.GuildVoiceStates,
+            IntentsBitField.Flags.GuildMessages,
+        ]});
         this.token = process.env.TOKEN as string;
         this.clientId = process.env.CLIENT_ID as string;
         this.disTube = new DisTube(this, this.disTubeOptions );
@@ -63,22 +80,35 @@ export class DiscordBot extends Client{
             console.error(error);
         }
     }
+    private registerDisTubeEvents(){
+        this.onError();
+        this.onInitQueue();
+        this.onPlaySong();
+        this.onSearchCancel();
+        this.onSearchResult();
+        this.onAddList();
+        this.onAddSong()
+    }
     private async init(){
         await this.registerCommands();
         this.login(this.token);
+        this.registerEvents();
+        this.registerDisTubeEvents();
+    }
+
+    private registerOnceEvent(){
         this.once(Events.ClientReady, c => {
             console.log(`Ready! Logged in as ${c.user.tag}`);
         });
+    }
+    private registerOnEvents(){
         this.on(Events.InteractionCreate, async interaction => {
             if (!interaction.isChatInputCommand()) return;
-
             const command = this.commands?.get(interaction.commandName);
-
             if (!command) {
                 console.error(`No command matching ${interaction.commandName} was found.`);
                 return;
             }
-
             try {
                 await command.execute(interaction, this);
             } catch (error) {
@@ -86,5 +116,79 @@ export class DiscordBot extends Client{
                 await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
             }
         });
+    }
+    private registerEvents(){
+        this.registerOnceEvent();
+        this.registerOnEvents();
+    }
+    private onSearchCancel(): void {
+        this.disTube.on('searchCancel', (message: Message) =>
+            message.channel.send(`**Searching canceled**`)
+        )
+    }
+    private onSearchResult(): void {
+        this.disTube.on('searchResult', (message: Message, result: Array<SearchResult>) => {
+            let i = 0;
+            message.channel.send(
+                `**Choose an option from below**\n${result
+                    .map(
+                        (song: SearchResult) =>
+                            `**${++i}**. ${song.name} 
+                            }\``
+                    )
+                    .join(
+                        '\n'
+                    )}\n*Enter anything else or wait 60 seconds to cancel*`
+            );
+        })
+    }
+    private status(queue: Queue): string {
+        return `Volume: ${queue.volume}% | Filter: ${queue.filters || 'Off'
+        } | Loop: ${queue.repeatMode
+            ? queue.repeatMode == 2
+                ? 'All Queue'
+                : 'This Song'
+            : 'Off'
+        } | Autoplay: ${queue.autoplay ? 'On' : 'Off'}`;
+    }
+    private onAddList(): void {
+        this.disTube.on('addList', (queue: Queue, playlist: Playlist) => {
+            const embed = new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle(':musical_note: | Added List')
+                .setDescription(`Added [${playlist.name}](${playlist.url}) playlist (${playlist.songs.length} songs) to queue`)
+            queue.textChannel?.send({embeds: [embed]});
+        })
+    }
+    private onAddSong(): void {
+        this.disTube.on('addSong', (queue: Queue, song: Song) => {
+            const embed = new EmbedBuilder()
+                .setColor(0x0099FF)
+                .setTitle(':musical_note: | Added Song')
+                .setDescription(`Added [${song.name}](${song.url}) to queue`)
+            queue.textChannel?.send({embeds: [embed]});
+        })
+    }
+    private onInitQueue(): void {
+        this.disTube.on('initQueue', (queue: Queue) => {
+            queue.autoplay = false;
+            queue.volume = 30;
+        });
+    }
+    private onError(): void {
+        this.disTube.on('error', (channel: GuildTextBasedChannel| undefined, e: Error) => {
+            if (channel) channel.send(`An error encountered: ${e}`)
+            else console.error(e)
+        })
+    }
+    private onPlaySong(): void {
+        this.disTube
+            .on('playSong', (queue: Queue, song: Song) => {
+                const embed = new EmbedBuilder()
+                    .setColor(0x0099FF)
+                    .setTitle(':play_pause: | Started Playing')
+                    .setDescription(` [${song.name}](${song.url}) - ${song.formattedDuration}\nRequested by: ${song.user}\n\n${this.status(queue)}`)
+                queue.textChannel?.send({embeds: [embed] });
+            })
     }
 }
---
+++ bot/src/commands/PlayCommand.ts
@@ -0,0 +1,49 @@
+
+import {
+    ChatInputCommandInteraction,
+    GuildMember,
+    GuildTextBasedChannel,
+    PermissionsBitField,
+    SlashCommandBuilder
+} from "discord.js";
+import {DiscordBot} from "../classes/DiscordBot";
+
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('play')
+        .setDescription('play something from yt, spotify etc')
+        .addStringOption(option =>
+            option
+                .setName('option')
+                .setDescription('seach option')
+        ),
+    async execute(interaction: ChatInputCommandInteraction , client: DiscordBot) {
+        try {
+            if (interaction.options.get("option")?.value) {
+                await interaction.reply(`🔍 **Searching...** \`${interaction.options.get("option")?.value}\``);
+
+                const member = interaction.member as GuildMember;
+                const channel = member?.voice?.channel;
+                if (!channel) return interaction.editReply("You need to be in voice channel.")
+                if (!channel.permissionsFor(interaction.guild?.members.me as GuildMember, false).has(PermissionsBitField.Flags.Connect)) return interaction.editReply(`I don't have perm \`CONNECT\` in ${channel.name} to join voice!`);
+                if (!channel.permissionsFor(interaction.guild?.members.me as GuildMember,false).has(PermissionsBitField.Flags.Speak)) return interaction.editReply(`I don't have perm \`SPEAK\` in ${channel.name} to join voice!`);
+
+                try {
+                    const options = {
+                        member: interaction.member as GuildMember,
+                        textChannel: interaction.channel as GuildTextBasedChannel,
+                        interaction,
+                    }
+                    const string = interaction.options.get("option")?.value as string;
+                    await client.disTube.play(channel, string, options)
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+        } catch (e) {
+            //
+        }
+    }
+}
